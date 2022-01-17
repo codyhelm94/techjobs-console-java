9859ddad7c415140bbaa85beceb0983183f479e9
codyhelm94 <cody.d.helm@gmail.com>
2022-01-17 17:17:41 -0600

    Finished Assignment

--- src/org/launchcode/techjobs/console/JobData.java
+++ src/org/launchcode/techjobs/console/JobData.java
@@ -7,9 +7,7 @@ import org.apache.commons.csv.CSVRecord;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by LaunchCode
@@ -76,7 +74,7 @@ public class JobData {
 
             String aValue = row.get(column);
 
-            if (aValue.contains(value)) {
+            if (aValue.toUpperCase().contains(value.toUpperCase())) {
                 jobs.add(row);
             }
         }
@@ -87,6 +85,21 @@ public class JobData {
     /**
      * Read in data from a CSV file and store it in a list
      */
+    public static ArrayList<HashMap<String, String>> findByValue(String value){
+        // load data, if not already loaded
+        loadData();
+
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+        for (HashMap<String, String> job : allJobs) {
+            for (Map.Entry<String, String> valuePair : job.entrySet()) {
+                if(valuePair.getValue().toUpperCase().contains(value.toUpperCase())){
+                    jobs.add(job);
+                    break;
+                }
+            }
+        }
+        return jobs;
+    }
     private static void loadData() {
 
         // Only load data once
